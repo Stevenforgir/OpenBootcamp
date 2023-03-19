4fec42083359eb0a04e161abfb5d639d28fe95c7
StevenUninorte <stevenforgir@gmail.com>
2023-03-19 17:36:49 -0500

    Ejercicio Tema 1

---
+++ Curso-Java-Basico/Tema_1_Conceptos/src/com/project/MyClass.java
@@ -0,0 +1,16 @@
+package com.project;
+
+public class MyClass {
+    public static void main(String[] args) {
+        int entero = 10;
+        long javaLong = 100000L;
+        double javaDouble = 1122235454;
+        boolean javaBoolean = true;
+        String javaString = "Este es un string en java";
+        System.out.println(entero);
+        System.out.println(javaLong);
+        System.out.println(javaDouble);
+        System.out.println(javaBoolean);
+        System.out.println(javaString);
+    }
+}

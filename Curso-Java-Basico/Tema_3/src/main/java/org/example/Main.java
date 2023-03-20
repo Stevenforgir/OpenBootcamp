76461771c9928073c3e41f8c098c1ec00787e9b2
StevenUninorte <stevenforgir@gmail.com>
2023-03-19 20:13:31 -0500

    Tema 3

---
+++ Curso-Java-Basico/Tema_3/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Daniela", "Kevin", "Camilo", "Laura"};
+        for(String nombre : nombres){
+            System.out.println(nombre);
+        }
+    }
+}

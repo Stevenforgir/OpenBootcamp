7fcc0b7b24fe5da52a14724ebf24d53b8479bc36
StevenUninorte <stevenforgir@gmail.com>
2023-04-02 19:56:44 -0500

    Ejercicio 1 curso Spring

---
+++ Curso_Spring/Ejercicios-1-2-3/Ejercicio1/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Crear proyecto maven con la dependencia spring-context y crear una clase Saludo con un método imprimirSaludo que imprima un texto por consola.
+ *
+ * Crear el archivo beans.xml con un bean para la clase Saludo.
+ *
+ * Obtener el bean desde el método main y ejecutar el método imprimirSaludo.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Saludo saludo = (Saludo) context.getBean("saludo");
+        saludo.imprimirSaludo();
+    }
+}

85070d6193ab2d2214bd9210c13050b9c875797e
StevenUninorte <stevenforgir@gmail.com>
2023-04-23 11:20:58 -0500

    Ejercicio 2

--- Curso_Spring/Ejercicios-1-2-3/Ejercicio1/src/main/java/org/example/Main.java
+++ Curso_Spring/Ejercicios-1-2-3/Ejercicio1/src/main/java/org/example/Main.java
@@ -3,17 +3,32 @@ package org.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
+/** Ejercicio 1
  * Crear proyecto maven con la dependencia spring-context y crear una clase Saludo con un método imprimirSaludo que imprima un texto por consola.
  *
  * Crear el archivo beans.xml con un bean para la clase Saludo.
  *
  * Obtener el bean desde el método main y ejecutar el método imprimirSaludo.
  */
+
+/** Ejercicio 2
+ * Crear la clase NotificationService, con un método que imprima un saludo.
+ *
+ * Crear una clase UserService que tenga un atributo de clase NotificationService.
+ *
+ * Utilizar la anotación @Component en cada clase.
+ *
+ * Habilitar el escaneo de clases en el archivo beans.xml
+ *
+ * Desde el método main, probar a obtener el bean UserService y ejecutar el método imprimirSaludo que tiene asociado el atributo de tipo NotificationService. De forma similar a la realizada en clase.
+ */
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Saludo saludo = (Saludo) context.getBean("saludo");
         saludo.imprimirSaludo();
+
+        UserService userService = context.getBean(UserService.class);
+        userService.enviarSaludo();
     }
 }
---
+++ Curso_Spring/Ejercicios-1-2-3/Ejercicio1/src/main/java/org/example/NotificationService.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+    public void imprimirSaludo() {
+        System.out.println("¡Hola desde NotificationService!");
+    }
+}

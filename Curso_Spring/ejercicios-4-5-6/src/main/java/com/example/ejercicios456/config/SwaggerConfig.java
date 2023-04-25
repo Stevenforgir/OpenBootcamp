a9240db3b05cef5f959b23baf9f06813f496b2ad
StevenUninorte <stevenforgir@gmail.com>
2023-04-24 22:05:14 -0500

    SwaggerConfig

---
+++ Curso_Spring/ejercicios-4-5-6/src/main/java/com/example/ejercicios456/config/SwaggerConfig.java
@@ -0,0 +1,78 @@
+package com.example.ejercicios456.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.FilterType;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * Configuración Swagger para la generación de documentación de la API REST
+ *
+ * HTML: http://localhost:8080/swagger-ui/
+ * JSON: http://localhost:8080/v2/api-docs
+*/
+
+@Configuration
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BasicErrorController.class)})
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Spring Boot Book API REST",
+                "Library Api rest docs",
+                "1.0",
+                "http://www.google.com",
+                new Contact("Alan", "http://www.google.com", "alan@example.com"),
+                "MIT",
+                "http://www.google.com",
+                Collections.emptyList());
+    }
+
+}
+/*@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.codmind.swaggerapi.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+                ;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Order Service API",
+                "Order Service API Description",
+                "1.0",
+                "http://codmind.com/terms",
+                new Contact("Codmind", "https://codmind.com", "apis@codmind.com"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
+    }
+}*/

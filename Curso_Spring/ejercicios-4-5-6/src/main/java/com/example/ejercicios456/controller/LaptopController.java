9689b35f6f3488368bdc26d018311bcdf0ab436d
StevenUninorte <stevenforgir@gmail.com>
2023-04-24 22:28:45 -0500

    Rest responses

--- Curso_Spring/ejercicios-4-5-6/src/main/java/com/example/ejercicios456/controller/LaptopController.java
+++ Curso_Spring/ejercicios-4-5-6/src/main/java/com/example/ejercicios456/controller/LaptopController.java
@@ -4,9 +4,11 @@ import com.example.ejercicios456.entity.LaptopModel;
 import com.example.ejercicios456.repository.LaptopRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/laptops/")
@@ -20,9 +22,38 @@ public class LaptopController {
         return laptopRepository.findAll();
     }
 
+    @ApiOperation("Traer laptop por id, no hay manejo de excepciones por el momento")
+    @GetMapping("find/{id}")
+    public Optional<LaptopModel> findLaptopById(@PathVariable Long id){
+        return laptopRepository.findById(id);
+    }
+
     @ApiOperation("Crear una laptop en DB")
     @PostMapping("/create")
-    public LaptopModel createLaptop(@RequestBody LaptopModel laptop) {
-        return laptopRepository.save(laptop);
+    public ResponseEntity<LaptopModel> createLaptop(@RequestBody LaptopModel laptop) {
+        if(laptop.getId() != null) {
+            return ResponseEntity.ok(laptopRepository.save(laptop));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<LaptopModel> updateLaptop(@RequestBody LaptopModel laptop) {
+        if(laptop.getId() != null) {
+            return ResponseEntity.ok(laptopRepository.save(laptop));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteLaptop(@PathVariable Long id){
+        try{
+            laptopRepository.findById(id);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+
+        laptopRepository.deleteById(id);
+        return ResponseEntity.ok("Elemento eliminado");
     }
 }

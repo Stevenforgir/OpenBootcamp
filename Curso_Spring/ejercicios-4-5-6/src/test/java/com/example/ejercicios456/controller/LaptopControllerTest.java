043e11213017d50787fe34b87c1de85ef9be6605
StevenUninorte <stevenforgir@gmail.com>
2023-04-29 13:36:05 -0500

    ejercicio9

---
+++ Curso_Spring/ejercicios-4-5-6/src/test/java/com/example/ejercicios456/controller/LaptopControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.ejercicios456.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopControllerTest {
+
+    @Test
+    void getLaptops() {
+    }
+
+    @Test
+    void findLaptopById() {
+    }
+
+    @Test
+    void createLaptop() {
+    }
+
+    @Test
+    void updateLaptop() {
+    }
+
+    @Test
+    void deleteLaptop() {
+    }
+}
